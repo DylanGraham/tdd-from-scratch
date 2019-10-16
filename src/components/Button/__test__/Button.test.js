caf9a944b4b7424c775f9428bbe3b7b9355d9e48
Janaka <chaminda.dissanayakemudeyanselage@anz.com>
2019-10-16 17:28:41 +1100

    Add Exe files

---
+++ src/components/Button/__test__/Button.test.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom/extend-expect'
+import Button from "../Button";
+
+it("should render as default button", () => {
+      const { container } = render(<Button />)
+      expect(container.firstChild).toMatchSnapshot();
+      
+});
